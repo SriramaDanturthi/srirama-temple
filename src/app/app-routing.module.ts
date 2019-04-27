4529abd7390625704d7e50fe8e8111a64d10d2c9
Sriram Danturthi <king.sriram.net@gmail.com>
2019-04-27 14:24:30 -0400

    board of directors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BoardOfDirectorsComponent } from './board-of-directors/board-of-directors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'board', component: BoardOfDirectorsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/board-of-directors/board-of-directors.component.ts
+++ src/app/board-of-directors/board-of-directors.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Person {
+  name: string;
+  role: string;
+}
+const BoardOfDirectors: Person[] = [
+  {
+    name: 'Sri. Archakam Padmanabhacharya',
+    role: 'Chairmen'
+  },
+  {
+    name: 'Sri. Krishnamohan',
+    role: 'Secretary'
+  },
+  {
+    name: 'Smt. Minal Patel',
+    role: 'Treasurer'
+  },
+  {
+    name: 'Sri. Mahesh Sachdeva',
+    role: 'Director'
+  }
+];
+
+
 @Component({
   selector: 'app-board-of-directors',
   templateUrl: './board-of-directors.component.html',
   styleUrls: ['./board-of-directors.component.scss']
-})
-export class BoardOfDirectorsComponent implements OnInit {
+}) export class BoardOfDirectorsComponent implements OnInit {
 
   constructor() { }
+  boardOfDirectors = BoardOfDirectors;
 
   ngOnInit() {
   }
-
 }
